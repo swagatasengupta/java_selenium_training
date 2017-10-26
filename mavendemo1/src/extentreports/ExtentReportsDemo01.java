225c5de6fe7095275b6abd4315e719f1106691c4
swagatasengupta <swagatasengupta@yahoo.com>
2017-10-27 01:34:59 +0530

    extent factory and extent report demo

---
+++ mavendemo1/src/extentreports/ExtentReportsDemo01.java
@@ -0,0 +1,16 @@
+package extentreports;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
+public class ExtentReportsDemo01 {
+
+	ExtentReports extRep;
+	ExtentTest extTest;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
