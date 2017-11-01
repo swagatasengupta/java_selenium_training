5bbd8eb668d8f2ec9827c6162c0bf634dcbe68b5
swagatasengupta <swagatasengupta@yahoo.com>
2017-11-02 01:51:49 +0530

    successful

--- mavendemo1/src/main/java/appium/App01.java
+++ mavendemo1/src/main/java/appium/App01.java
@@ -7,23 +7,27 @@ import java.net.URL;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
 
 public class App01 {
 
 	public static void main(String[] args) {
-		AndroidDriver driver;
+		AndroidDriver<AndroidElement> driver;
 		File app = new File("C:\\Users\\Swagat\\Downloads\\API Demos for Android_v1.9.0_apkpure.com.apk");
 		DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("device","Android");
-
+        //capabilities.setCapability("device","Android");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"SwagatNexus5XAPI27");
+        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\Swagat\\Downloads\\ApiDemos-debug.apk");
+        System.out.println(capabilities.toString());
         //mandatory capabilities
-        capabilities.setCapability("deviceName","Swagat Sengupta (Galaxy Note3)");
+/*        capabilities.setCapability("deviceName","Swagat Sengupta (Galaxy Note3)");
         capabilities.setCapability("platformName","Android");
-
+*/
         //other caps
-        capabilities.setCapability("app", app.getAbsolutePath());
-        try {
-			driver =  new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+/*        capabilities.setCapability("app", app.getAbsolutePath());
+*/        try {
+			driver =  new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
