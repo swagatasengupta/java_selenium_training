39e308338cd8d7211b52f1c6d012300f23ba954e
swagat <swagat@DESKTOP-NH5RR79>
2018-03-21 12:55:30 +0530

    updated with more comments

--- mavendemo1/src/main/java/database/TestMySQLDB.java
+++ mavendemo1/src/main/java/database/TestMySQLDB.java
@@ -10,17 +10,24 @@ public class TestMySQLDB {
 
 	public static void main(String[] args) {
 
-		String hostName = "localhost";
-		int port = 3306;
-		String userName = "root";
-		String password = "password";
-		String dbName = "test_schema01";
+		String hostName = "localhost"; //change if host is different
+		int port = 3306; // change if port is different
+		String userName = "root"; //change if DB user name is different
+		String password = "password"; // change if DB password for user name is different
+		String dbName = "test_schema01"; //change if DB schema is different.
+		String query = "select * from creds"; //change the select query if needed. Take care of the table name etc.
+		
+		
+		//this statement will form the connect string. Dont disturb this unless you are really sure of what you are doing.
 		String connString = "jdbc:mysql://"
 				+ hostName + ":" + port + "/"
 				+ dbName + "?"
 				+ "user="+userName+"&"
 				+ "password="+password;
 		//"jdbc:mysql://localhost/feedback?" + "user=sqluser&password=sqluserpw"
+		
+		
+		
 		System.out.println("attempting to connect:: " + connString);
 		Connection connection = null;
 		ResultSet res = null;
@@ -31,7 +38,8 @@ public class TestMySQLDB {
 	        connection = DriverManager.getConnection(connString);
 	        Statement st = connection.createStatement();                                 
 	        System.out.println("connection succeeded");
-	        String query = "select * from creds";
+	        
+
 	        res = st.executeQuery(query);
 	        while(res.next()) {
 	        	int cols = res.getMetaData().getColumnCount();
